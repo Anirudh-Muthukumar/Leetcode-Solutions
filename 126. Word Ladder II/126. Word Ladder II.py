454fdb04be7324f69d5b8813207bf19ce9454baa
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-06-16 10:24:17 -0400

    June Challenge

--- 126. Word Ladder II/126. Word Ladder II.py
+++ 126. Word Ladder II/126. Word Ladder II.py
@@ -1,39 +1,63 @@
 import collections
 
 class Solution:
-    def __init__(self):
-        self.graph = collections.defaultdict(list)
-        self.n = 0
-        self.res = []
-        
     def findLadders(self, beginWord, endWord, wordList):
-        if not beginWord or not endWord or endWord not in wordList or not wordList:
-            return []
         
-        self.n = len(beginWord)
+        if endWord not in wordList:
+            return [] 
+        graph = collections.defaultdict(list)
+        n = len(beginWord)
         
         for word in wordList:
-            for i in range(self.n):
-                self.graph[word[:i] + '*' + word[i+1:]].append(word)
+            for i in range(n):
+                graph[word[:i] + '*' + word[i+1:]].append(word)
         
-        queue = collections.deque([(beginWord, [beginWord])])  
-        visited = set([beginWord])
+        res = []
+        queue_start = collections.deque([beginWord])
+        queue_end = collections.deque([endWord])
+        visited_start = set([beginWord])
+        visited_end = set([endWord])
+        parents = collections.defaultdict(set)
         found = False
+        depth = 0
+        rev = False
         
-        while queue and not found:
-            size = len(queue)
-            localvisited = set()
+        while queue_start and not found:
+            depth += 1
+            size = len(queue_start)
+            localVisited = set()
             for _ in range(size):
-                current_word, path = queue.popleft()
-                for i in range(self.n):
-                    intermediate_word = current_word[:i] + '*' + current_word[i+1:]
-                    for word in self.graph[intermediate_word]:
-                        if word==endWord:
-                            found = True
-                            self.res += path + [endWord],
-                        if word not in visited:
-                            localvisited.add(word)
-                            queue += (word, path + [word]),
-            visited = visited.union(localvisited)
+                word = queue_start.popleft()
+                for i in range(n):
+                    for nextWord in graph[word[:i] + '*' + word[i+1:]]:
+                        if nextWord not in visited_start:
+                            localVisited.add(nextWord)
+                            if nextWord in visited_end:
+                                found = True
+                            queue_start.append(nextWord)
+                            if not rev:
+                                parents[nextWord].add(word)
+                            else:
+                                parents[word].add(nextWord)
+                            
+            visited_start = visited_start.union(localVisited)
+            
+            queue_start, queue_end = queue_end, queue_start
+            visited_start, visited_end = visited_end, visited_start
+            rev = not rev
+            
+                                        
+        def dfs(word, path, d):
+            if d==0:
+                if path[-1]==beginWord:
+                    res.append(path[::-1])
+                return
+            
+            for parent in parents[word]:
+                dfs(parent, path + [parent], d-1)
         
-        return self.res
+        dfs(endWord, [endWord], depth)
+        
+        return res
+
+                
