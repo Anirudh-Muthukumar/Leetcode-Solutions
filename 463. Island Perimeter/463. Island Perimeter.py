9ff53c818488e020337c5944689d7ea4c9d34035
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-07 18:09:55 -0400

    Minor

--- 463. Island Perimeter/463. Island Perimeter.py
+++ 463. Island Perimeter/463. Island Perimeter.py
@@ -1,35 +1,40 @@
+import collections
+
 class Solution:
     def islandPerimeter(self, grid):
-        if grid==[]:
-            return 0
-        
-        dfs = {}
+        def getPerimeter(x, y):
+            res = 0
+            for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                if 0<=i<m and 0<=j<n and grid[i][j]==0:
+                    res += 1
+                elif not 0<=i<m or not 0<=j<n:
+                    res += 1
+            return res
+            
         m, n = len(grid), len(grid[0])
-        perimeter = 0
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        x, y = -1, -1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    x, y = i, j
+                    break
         
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c]==1:
-                    q = [(r, c)]
+        perimeter = 0
+        q = collections.deque([(x, y)])
+        visited = {}
+        visited[(x, y)] = 1
+        
+        while q:
+            x, y = q.popleft()
+            
+            # Calculate and perimeter
+            perimeter += getPerimeter(x, y)
+            
+            for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                if 0<=i<m and 0<=j<n and (i, j) not in visited and grid[i][j]==1:
+                    visited[(i, j)] = 1
+                    q.append((i, j))
                     
-                    while q:
-                        (x, y) = q.pop()  
-                        if visited[x][y]:
-                            continue
-                        visited[x][y] = True
-                        
-                        
-                        for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-                            if 0<=i<m and 0<=j<n and visited[i][j]==False and grid[i][j]==1:
-                                q.append((i, j))
-                            elif not 0<=i<m or not 0<=j<n or grid[i][j]==0:
-                                perimeter += 1 
-                        
-                    return perimeter
         
         return perimeter
-                                
-        
-        
-        
+                    
