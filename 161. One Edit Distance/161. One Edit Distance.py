7f579e92114cce7f18d89ea5aba2a9ec1a301afe
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-16 11:52:24 -0400

    July Challenge

--- 161. One Edit Distance/161. One Edit Distance.py
+++ 161. One Edit Distance/161. One Edit Distance.py
@@ -1,59 +1,26 @@
 '''
-Time Complexity : O(m+n)
-Space Complexity : O(1)
+Time Complexity : O(n)
+Space Complexity : O(n)
 '''
 
 class Solution:
     def isOneEditDistance(self, x, y):
-        def linearSpace():
-            m, n = len(x), len(y)
-            first = [j for j in range(n+1)]
-            
-            for i in range(1, m+1):
-                second = [0 for _ in range(n+1)]
-                second[0] = i
-                for j in range(1, n+1):
-                    if x[i-1]==y[j-1]:
-                        second[j] = first[j-1]
-                    else:
-                        second[j] = 1 + min(first[j], second[j-1], first[j-1])
-                
-                first = second[::]
-                
-            return first[n]==1
+        # x is smaller than y
+        m, n = len(x), len(y)
         
-        def constantSpace():
-            m, n = len(x), len(y)
-            dist, i, j = 0, 0, 0
-
-            while i<m and j<n:
-                if x[i]==y[j]:
-                    i+=1
-                    j+=1
+        if m > n:
+            return self.isOneEditDistance(y, x)
+        
+        if n - m > 1:
+            return False
+        
+        for i in range(m):
+            if x[i]!=y[i]:
+                if m==n: 
+                    return x[i+1:]==y[i+1:]
                 else:
-                    dist += 1
-
-                    if dist>1:
-                        return False 
-                    
-                    if (m-i)==(n-i): # replace 
-                        i += 1
-                        j += 1
-                    elif (m-i) > (n-j): #insert
-                        j+=1 
-                    else: # delete
-                        i+=1
-            
-            return dist==1
+                    return x[i:]==y[i+1:]
         
-        return constantSpace()
-        return linearSpace()
-            
+        return m+1==n
 
-if __name__ == '__main__':
-    soln = Solution()
-    # X = 
-    # X = "geetcode"
-    # Y = "leetcode"
-    print(soln.oneEditDistance('1203', '1213'))
-#     print(oneEditDistance(X, Y))
+            
