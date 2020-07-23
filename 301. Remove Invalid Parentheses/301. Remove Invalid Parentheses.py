eb917022039c0d183d0ca722b0d748ef8c56f3fb
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-23 17:37:48 -0400

    DFS, BFS

--- 301. Remove Invalid Parentheses/301. Remove Invalid Parentheses.py
+++ 301. Remove Invalid Parentheses/301. Remove Invalid Parentheses.py
@@ -1,36 +1,46 @@
+import collections
+
 class Solution:
-    def removeInvalidParentheses(self, s) :
+    def removeInvalidParentheses(self, s):
         
-        def validParentheses(state):
+        def valid(s):
             stack = []
-            for ch in state:
+            for ch in s:
                 if ch=='(':
                     stack.append(ch)
                 elif ch==')':
                     if len(stack)>0 and stack[-1]=='(':
-                        stack.pop() 
+                        stack.pop()
                     else:
                         return False
             return len(stack)==0
-
-        q = [(s, 0)]
-        visited = set([s])
-        min_dist = float('inf')
-        res = []
-        while q:
-            state, dist = q.pop(0) 
-
-            if validParentheses(state):
-                if dist <= min_dist:
-                    res.append(state)
-                    min_dist = dist
-                else:
-                    break
-
-            for i in range(len(state)):
-                new_state = state[:i]+state[i+1:]
-                if new_state not in visited:
-                    visited.add(new_state)
-                    q.append((new_state, dist+1))
+                    
         
+        q = collections.deque([(s, 0)])
+        mindist = float('inf')
+        visited = set([s])
+        setBracket = set(['(',')'])
+        res = []
+        
+        while q:
+            state, ct = q.popleft()
+            
+            if ct > mindist:
+                return res
+            
+            if valid(state):
+                if ct <= mindist:
+                    mindist = ct
+                    res.append(state)
+                
+            for i in range(len(state)):
+                if state[i] in setBracket:
+                    new_state = state[:i]+state[i+1:]
+                    if new_state not in visited:
+                        visited.add(new_state)
+                        q.append((new_state, ct+1))
+                    
         return res
+            
+        
+        
