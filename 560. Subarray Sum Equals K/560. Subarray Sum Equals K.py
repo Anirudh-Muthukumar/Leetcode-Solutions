368d6d9ee2254df9e277c2a0665f2587a7dd9d46
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-24 03:29:39 -0400

    Arrays, Maps

--- 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
@@ -1,9 +1,14 @@
+import collections
+
 class Solution:
-    def subarraySum(self, nums, k: int):
-        curr, ans = 0, 0
-        mp = {0:1}
-        for i in range(len(nums)):
-            curr += nums[i] 
-            ans += mp.get(curr-k, 0) 
-            mp[curr] = mp.get(curr, 0) + 1 
-        return ans
+    def subarraySum(self, A, k):
+        mp = collections.defaultdict(int)
+        mp[0] = 1
+        curr = 0
+        res = 0
+        for x in A:
+            curr += x
+            res += mp.get(curr-k, 0)
+            mp[curr] += 1
+        return res
+        
--- 581. Shortest Unsorted Continuous Subarray/581. Shortest Unsorted Continuous Subarray.py
+++ 581. Shortest Unsorted Continuous Subarray/581. Shortest Unsorted Continuous Subarray.py
@@ -1,28 +1,38 @@
-def findUnsortedSubarray(self, nums):
+class Solution:
+    def findUnsortedSubarray(self, A):
+        n = len(A)
         
-        n = len(nums)
+        # Find the min and max in the unsorted array
+        min_elem, max_elem = float('inf'), float('-inf')
+        started = False
+        for i in range(1, n):
+            if A[i] < A[i-1]: # unsorted array has started
+                started = True
+                if started:
+                    min_elem = min(min_elem, A[i])
         
-        if n<=1:
+        if not started: # array is sorted
             return 0
-
-        lo = 0
-        hi = len(nums)-1
         
-        # find the position of lo
-        while lo<n and nums[lo]==min(nums[lo:]):
-            lo += 1
-        
-        if lo==n:
-            return 0
+        started = False
+        for i in range(n-2, -1, -1):
+            if A[i] > A[i+1]: # unsorted array has started
+                started = True
+                if started:
+                    max_elem = max(max_elem, A[i])
         
 
-        # find the position of hi
-        while hi>=0 and nums[hi]==max(nums[:hi+1]):
-            hi -= 1
-
+        l, r = 0, n-1
         
-        if hi==len(nums)-1 and lo==0:
-            return n
+        # find correct positions of min and max_elem
+        for i in range(n):
+            if A[i] > min_elem:
+                l = i
+                break
         
-        return hi-lo+1
-
+        for i in range(n-1, -1, -1):
+            if A[i] < max_elem:
+                r = i
+                break
+        
+        return r-l+1
