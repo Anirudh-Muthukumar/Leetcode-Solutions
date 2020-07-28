fe8eed55258d4922bd6b38aa66235cca4915b2c0
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-28 12:52:21 -0400

    DFS, Hash

--- 199. Binary Tree Right Side View/199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View/199. Binary Tree Right Side View.py
@@ -1,27 +1,32 @@
+import collections
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def rightSideView(self, root):
         if not root:
             return []
-        
-        q = [(root, 1)]
-        
+        q = collections.deque([(root, 1)])
         res = []
-        
         while q:
-            node, depth = q.pop(0)
-            
-            if len(res) < depth : # first node of each level
-                res += node.val,
-            
-            if node.right:
-                q += (node.right, depth+1), # visit right child first
-            if node.left:
-                q += (node.left, depth+1),
-            
+            size = len(q)
+            max_index = -1
+            right_most = None
+            for _ in range(size):
+                node, index = q.popleft()
+                if index > max_index:
+                    max_index = index
+                    right_most = node.val
+                if node.left:
+                    q.append((node.left, index*2))
+                if node.right:
+                    q.append((node.right, index*2+1))
+            res.append(right_most)
+        
         return res
+        
--- 621. Task Scheduler/621. Task Scheduler.py
+++ 621. Task Scheduler/621. Task Scheduler.py
@@ -1,22 +1,19 @@
+import collections
+
 class Solution:
     def leastInterval(self, tasks, n):
-        
-        mp = [0] * 26
-        
+        frequencies = [0] * 26
         for task in tasks:
-            mp[ord(task) - ord('A')] += 1
-        
-        mp.sort(reverse = True)  # 26log(26)
-        
-        max_val = mp[0] - 1
-        idle_slots = max_val * n
-        
-        for i in range(1, 26): # first task is already accounted
-            idle_slots -= min(max_val, mp[i])
-        
-        
-        if idle_slots > 0:
-            return idle_slots + len(tasks)
-        
-        return len(tasks)
+            frequencies[ord(task)-ord('A')] += 1
             
+        frequencies.sort()
+        f_max = frequencies.pop()
+        idle_slots = (f_max - 1) * n
+        
+        while frequencies and idle_slots > 0:
+            idle_slots -= min(f_max-1, frequencies.pop())
+            
+        idle_slots = max(0, idle_slots)
+        return idle_slots + len(tasks)
+        
+        
