b80bf5b588721ed671d64e7bbccd41fa28fa2461
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-07-28 18:05:25 -0400

    Binary Search

--- 274. H-Index/274. H-Index.py
+++ 274. H-Index/274. H-Index.py
@@ -1,33 +1,21 @@
 class Solution:
-    def hIndex(self, A):
-        if A==[]:
+    def hIndex(self, citations):
+        
+        if not citations:
             return 0
         
-        A.sort()
+        def getCitations(h):
+            return sum(1 for x in citations if x>=h)
         
-        def getCitations(x):
-            i, j = 0, n-1
-            while i<j:
-                mi = (i+j)>>1
-                if A[mi] < x:
-                    i = mi + 1
-                else:
-                    j = mi
-            return n-i
-                    
-        n = len(A)
-        l = 0
-        r = max(A)
-        
+        l, r = 0, len(citations)
+        candidate = 1
         while l<=r:
-            mid = (l+r)>>1           
-            citations_mid = getCitations(mid)
-            citations_mid_1 = getCitations(mid+1)
-            if  citations_mid >= mid and citations_mid_1 < (mid+1):
-                return mid
-            if mid <= citations_mid:
+            mid = (l+r)>>1
+            no_citations = getCitations(mid)
+            if no_citations >= mid:
+                candidate = mid
                 l = mid + 1
             else:
-                r = mid
+                r = mid - 1
         
-        return l-1
+        return candidate
