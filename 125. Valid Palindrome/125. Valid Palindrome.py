12d5d11838812aca1418926fd26b4bc31777a0ff
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-08-03 11:33:52 -0400

    Strings

--- 125. Valid Palindrome/125. Valid Palindrome.py
+++ 125. Valid Palindrome/125. Valid Palindrome.py
@@ -1,17 +1,21 @@
+'''
+Time complexity : O(n)
+Space complexity: O(1)
+'''
+
 class Solution:
     def isPalindrome(self, s):
-        l, r = 0, len(s)-1
-        
-        while l<r:
-            while l<r and not s[l].isalnum():
-                l+=1
-            while l<r and not s[r].isalnum():
-                r-=1
-            if s[l].lower()!=s[r].lower():
+        n = len(s)
+        i, j = 0, n-1
+        while i<j:
+            while i<j and not s[i].isalnum():
+                i += 1
+            while i<j and not s[j].isalnum():
+                j -= 1
+                
+            if s[i].lower()!=s[j].lower():
                 return False
-            l+=1
-            r-=1
-        
+            i += 1
+            j -= 1
+            
         return True
-        
-        
