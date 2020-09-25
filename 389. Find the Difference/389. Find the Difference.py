0b87a40ddee6753b65f338d5bb35f417464da2b5
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-09-24 20:32:36 -0400

    Array

--- 389. Find the Difference/389. Find the Difference.py
+++ 389. Find the Difference/389. Find the Difference.py
@@ -1,8 +1,4 @@
-class Solution:
-    def findTheDifference(self, s, t):
-        missing = 0
-        for ch in s:
-            missing ^= ord(ch)
-        for ch in t:
-            missing ^= ord(ch)
-        return chr(missing)
+'''
+Time complexity : O(n)
+Space complexity: O(1) array of size 26
+'''
