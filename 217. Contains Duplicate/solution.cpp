0ef3a2d8140e44b88f8ba7fd538cdc167508247f
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-10-19 14:41:29 -0400

    Arrays

--- 217. Contains Duplicate/solution.cpp
+++ 217. Contains Duplicate/solution.cpp
@@ -11,11 +11,25 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(begin(nums), end(nums));
-        for(int i = 1; i < n; ++i)
-            if(nums[i] == nums[i-1])
-                return true;
-        return false;
+        return constantSpace(nums);
     }
+private:
+  bool constantSpace(vector<int>& A){
+      int n = A.size();
+      sort(begin(A), end(A));
+      for(int i = 1; i < n; ++i)
+          if(A[i] == A[i-1])
+              return true;
+      return false;
+  }  
+  bool linearTime(vector<int>& A){
+      set<int> seen;
+      int n = A.size();
+      for(int i = 0; i < n; ++i){
+          if(seen.count(A[i]))
+              return true;
+          seen.insert(A[i]);
+      }
+      return false;
+  }
 };
