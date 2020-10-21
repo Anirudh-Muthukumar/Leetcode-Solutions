51ade04d05aaf5e9be87bed0933ef133ccea6db9
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2020-10-21 01:22:55 -0400

    Binary Tree

--- 100. Same Tree/100. Same Tree.py
+++ 100. Same Tree/100. Same Tree.py
@@ -1,3 +1,11 @@
+"""
+Idea: Check each node and recur for both the child.
+
+Time complexity : O(n)
+Space complexity: O(n)
+"""
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
