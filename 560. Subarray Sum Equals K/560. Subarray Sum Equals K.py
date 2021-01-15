0c68642b6b5c75edc44214227a9a79aa9bf8eba6
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2021-01-16 03:07:55 +0530

    DP

--- 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
@@ -2,16 +2,21 @@
 Time complexity: O(n)
 Space complexity: O(n)
 '''
+import collections
 
 class Solution:
-    def subarraySum(self, A, k):
-        mp = {0: 1}
-        n = len(A)
-        curr = 0
+    def subarraySum(self, nums, k):
+        
+        n = len(nums)
         res = 0
+        seen = collections.defaultdict(int)
+        seen[0] = 1
+        curr_sum = 0
+        
         for i in range(n):
-            curr += A[i]
-            res += mp.get(curr-k, 0)
-            mp[curr] = mp.get(curr, 0) + 1
+            curr_sum += nums[i]
+            res += seen[curr_sum - k]
+            seen[curr_sum] += 1 
+        
         return res
         
