36c3c6ed5399842c59da49cb93633f890ba3309e
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2021-01-23 02:12:04 +0530

    Deque

--- 1425. Constrained Subset Sum/1425. Constrained Subset Sum.py
+++ 1425. Constrained Subset Sum/1425. Constrained Subset Sum.py
@@ -1,38 +1,29 @@
+"""
+Idea: Use deque
+
+Time complexity: O(n)
+Space complexity: O(k)
+
+"""
+
 import collections
 
-class Monoqueue(collections.deque):
-    def enqueue(self, new_val):
-        count = 1   
-        while self and self[-1][0] < new_val: # make sure first element is the largest 
-            count += self.pop()[1]
-        self.append([new_val, count])
-    
-    def dequeue(self):
-        ans = self.max()
-        self[0][1] -= 1    # decrement its count
-        if self[0][1] <= 0:
-            self.popleft()
-        return ans
-    
-    def max(self):
-        if self:
-            return self[0][0]
-        return 0
+class Solution:
+    def constrainedSubsetSum(self, A, k):
 
-
-def constrainedSubsetSum(self, A, k):
-    queue = Monoqueue()
-    ans = max(A)
-    
-    for i, x in enumerate(A):
-        queue.enqueue(x + max(0, queue.max()))
-        if i>=k :   # update window
-            ans = max(ans, queue.dequeue())
-    
-    return max(ans, queue.dequeue())
+        n = len(A)
+        q = collections.deque()
         
-
-
-nums = [10,-2,-10,-5,20]
-k = 2
-print(constrainedSubsetSum(nums, k))
+        for i in range(n):
+            A[i] += q[0] if q else 0
+            
+            while q and A[i] > q[-1]:
+                q.pop()
+            
+            if A[i] > 0:
+                q.append(A[i])
+            
+            if i >= k and q and q[0] == A[i-k]:
+                q.popleft()
+        
+        return max(A)
