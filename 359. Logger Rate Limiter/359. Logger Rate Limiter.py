e7f3fd9cda55dfdb28977473df484ee9a2adbbd1
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2021-03-05 10:56:34 -0500

    Google

--- 359. Logger Rate Limiter/359. Logger Rate Limiter.py
+++ 359. Logger Rate Limiter/359. Logger Rate Limiter.py
@@ -3,32 +3,16 @@ import collections
 class Logger:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.timestamps = collections.deque()
-        self.messages = collections.deque()
-
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
-
-        while self.timestamps and self.timestamps[-1] <= timestamp - 10:
-            self.timestamps.pop()
-            self.messages.pop()
-        if message in self.messages:
-            res = False
-        else:
-            res = True
-            self.timestamps.appendleft(timestamp)
-            self.messages.appendleft(message)
-    
-        return res
+        self.q = collections.defaultdict(int)
         
 
+    def shouldPrintMessage(self, timestamp, message):
+        if message in self.q and self.q[message] > timestamp:
+            return False
+        
+        self.q[message] = timestamp + 10
+        return True
+        
 
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
