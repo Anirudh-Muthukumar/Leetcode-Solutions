6263091008b2a74f23195bb112f9161b77146cc2
Anirudh-Muthukumar <anirudhmuthu@umass.edu>
2021-04-06 00:16:42 -0400

    Jan-May

--- 304. Range Sum Query 2D - Immutable/304. Range Sum Query 2D - Immutable.py
+++ 304. Range Sum Query 2D - Immutable/304. Range Sum Query 2D - Immutable.py
@@ -1,20 +1,51 @@
+"""
+Idea: Binary Indexed Tree
+
+Time complexity : O(log(m*n))    for any query
+Space complexity: O(m * n)
+"""
+
 class NumMatrix:
 
     def __init__(self, A):
-        if len(A)==0:
-            return 
-        m, n = len(A), len(A[0])
-        self.rangeSum = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        m = self.m = len(A)
+        if m == 0:
+            return None
+        n = self.n = len(A[0])
+        self.BIT = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        self.A = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m):
+            for j in range(n):
+                self.update(i, j, A[i][j])
+    
+    
+    def update(self, row, col, val):
+        diff = val - self.A[row + 1][col + 1]
+        self.A[row + 1][col + 1] = val
+        i = row + 1
+        while i <= self.m:
+            j = col + 1
+            while j <= self.n:
+                self.BIT[i][j] += diff
+                j += (j & -j)
+            i += (i & -i)
         
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                self.rangeSum[i][j] = self.rangeSum[i-1][j] + self.rangeSum[i][j-1] - self.rangeSum[i-1][j-1] + A[i-1][j-1]
-            
+    
+    def get(self, row, col):
+        res = 0
+        i = row + 1
+        while i > 0:
+            j = col + 1
+            while j > 0:
+                res += self.BIT[i][j]
+                j -= (j & -j)
+            i -= (i & -i)
+        
+        return res
+    
 
-    def sumRegion(self, p, q, r, s):
-        p, q, r, s = p+1, q+1, r+1, s+1
-        return self.rangeSum[r][s] - self.rangeSum[r][q-1] - self.rangeSum[p-1][s] + self.rangeSum[p-1][q-1]
-        
+    def sumRegion(self, row1, col1, row2, col2):
+        return self.get(row2, col2) - self.get(row2, col1 - 1) - self.get(row1 - 1, col2) + self.get(row1 - 1, col1 - 1)
 
 
 # Your NumMatrix object will be instantiated and called as such:
